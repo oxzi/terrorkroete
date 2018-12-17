7de486eff1a57339f90656f75e100cb6baca4ce6
Alvar Penning <post@0x21.biz>
2018-12-17 21:09:45 +0100

    Stop flickering after music stops

--- src/main.cpp
+++ src/main.cpp
@@ -10,18 +10,19 @@
 
 #include "bullenwagen.h"
 
+void flickering(void*);
+
 AudioFileSourcePROGMEM *in;
 AudioGeneratorMP3 *mp3;
 AudioOutputI2SNoDAC *out;
+
+QueueHandle_t queue;
 
 int8_t leds[5] = { 16, 17, 4, 2, 15 };
 int8_t eyes[2] = { 5, 18 };
 int8_t button = 21;
 
-void flickering(void*);
-
-void setup()
-{
+void setup() {
   for (int8_t i = 0; i < sizeof(leds); i++) {
     pinMode(leds[i], OUTPUT);
   }
@@ -33,8 +34,9 @@ void setup()
 
   pinMode(button, INPUT);
 
-  xTaskCreatePinnedToCore(
-    flickering, "flickering", 10000, NULL, 0, NULL, 0);
+  queue = xQueueCreate(1, sizeof(bool));
+
+  xTaskCreate(flickering, "flickering", 10000, NULL, 0, NULL);
 
   in = new AudioFileSourcePROGMEM(bullenwagen_mp3, bullenwagen_mp3_len);
   mp3 = new AudioGeneratorMP3();
@@ -43,16 +45,27 @@ void setup()
   mp3->begin(in, out);
 }
 
-void flickering(void *pvParams) {
-  for (int8_t i = 0;; i = random(0, sizeof(leds))) {
+void flickering(void *param) {
+  for (int8_t i = 0; uxQueueMessagesWaiting(queue) == 0; i = random(0, sizeof(leds))) {
     digitalWrite(leds[i], random(2));
-    delay(random(10, 100));
+    delay(random(15, 50));
   }
+
+  for (int8_t i = 0; i < sizeof(leds); i++) {
+    digitalWrite(leds[i], LOW);
+  }
+
+  for (;;);
 }
 
-void loop()
-{
+void loop() {
+  bool msg = true;
+
   if (mp3->isRunning()) {
-    mp3->loop();
+    if (!mp3->loop()) {
+      mp3->stop();
+    }
+  } else {
+    xQueueSend(queue, (void *)&msg, (TickType_t) 0);
   }
 }
